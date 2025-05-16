400fcbcca4cc1cc8d11f34653544722cf1bcb06d
c-bruce <callum.bruce1@gmail.com>
2025-05-16 19:32:15 +0100

    Transmitting SensorData

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,31 @@
 #include <Arduino.h>
 
+// void setup() {
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   Serial.println("H");
+//   delay(1000);
+//   Serial.println("L");
+//   delay(1000);
+// }
+
+struct SensorData {
+  int temperature;
+  float voltage;
+};
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("H");
-  delay(1000);
-  Serial.println("L");
-  delay(1000);
+  SensorData data;
+  data.temperature = 25;
+  data.voltage = 3.7;
+
+  Serial.write((uint8_t *)&data, sizeof(data));
+
+  delay(2000);
 }
