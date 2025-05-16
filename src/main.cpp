7eaa5c17c2e2a9e782d488428842f04f974bfc4a
c-bruce <callum.bruce1@gmail.com>
2025-05-16 19:56:23 +0100

    Updated SensorData to contain BPM-280, MPU-6050 and GPS data

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,23 @@
 // }
 
 struct SensorData {
-  int temperature;
-  float voltage;
+  // BPM-280
+  float temperature;
+  float pressure;
+  float altitude;
+  // MPU-6050
+  float accX;
+  float accY;
+  float accZ;
+  float gyroX;
+  float gyroY;
+  float gyroZ;
+  // GPS
+  float latitude;
+  float longitude;
+  float altitudeGPS;
+  float speed;
+  float course;
 };
 
 SoftwareSerial loraSerial(2, 3); // RX, TX
@@ -26,8 +41,20 @@ void setup() {
 
 void loop() {
   SensorData data;
-  data.temperature = 25;
-  data.voltage = 3.7;
+  data.temperature = 1.1;
+  data.pressure = 1.2;
+  data.altitude = 1.3;
+  data.accX = 2.1;
+  data.accY = 2.2;
+  data.accZ = 2.3;
+  data.gyroX = 2.4;
+  data.gyroY = 2.5;
+  data.gyroZ = 2.6;
+  data.latitude = 3.1;
+  data.longitude = 3.2;
+  data.altitudeGPS = 3.3;
+  data.speed = 3.4;
+  data.course = 3.5;
 
   loraSerial.write((uint8_t *)&data, sizeof(data));
 
